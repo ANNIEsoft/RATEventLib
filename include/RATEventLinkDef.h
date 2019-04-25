f2803289aee51fb385d0fc698e3524904a566800
Teal Pershing <tjpershing@ucdavis.edu>
2019-04-25 07:13:08 -0500

    Compiles on cluster and libraries can load successfully

--- include/RATEventLinkDef.h
+++ include/RATEventLinkDef.h
@@ -1,15 +1,24 @@
-#ifdef __CINT__
+#if defined(__CLING__) || defined(__CINT__)
 
 #pragma link off all globals;
 #pragma link off all classes;
 #pragma link off all functions;
 
-//#pragma link C++ nestedtypedef;
-//#pragma link C++ nestedclass;
+#pragma link C++ nestedtypedef;
+#pragma link C++ nestedclass;
+
+#pragma line C++ namespace RAT;
+#pragma line C++ namespace DS;
+#pragma line C++ namespace RAT::DS;
+//
+#pragma line C++ defined_in namespace RAT;
+#pragma line C++ defined_in namespace DS;
+#pragma line C++ defined_in namespace RAT::DS;
 
 #pragma link C++ class PMTInfo+;
 #pragma link C++ class LAPPDInfo+;
 
+#pragma link C++ class RAT::DS::Root+;
 #pragma link C++ class RAT::DS::MC+;
 #pragma link C++ class RAT::DS::MCParticle+;
 #pragma link C++ class RAT::DS::MCTrack+;
@@ -39,46 +48,10 @@
 #pragma link C++ class RAT::ObjDbl+;
 #pragma link C++ class RAT::Log+;
 #pragma link C++ class RAT::DBFieldCallback+;
+#pragma link C++ class RAT::TrackCursor+;
+#pragma link C++ class RAT::TrackNode+;
+#pragma link C++ class RAT::TrackNav+;
 
 #endif // __CINT__
 
-#ifdef __MAKECINT__
-
-#pragma link C++ class pair<string,string>+;
-#pragma link C++ class map<int, int>+;
-#pragma link C++ class map<int, vector<int> >+;
-#pragma link C++ class vector<TVector3>;
-#pragma link C++ class vector<TLorentzVector>; 
-#pragma link C++ class vector<RAT::DS::Root>;
-#pragma link C++ class vector<PMTInfo>;
-#pragma link C++ class vector<LAPPDInfo>;
-
-#pragma link C++ class vector<RAT::DS::MC>;
-#pragma link C++ class vector<RAT::DS::MCParticle>;
-#pragma link C++ class vector<RAT::DS::MCTrack>;
-#pragma link C++ class vector<RAT::DS::MCTrackStep>;
-#pragma link C++ class vector<RAT::DS::MCPMT>;
-#pragma link C++ class vector<RAT::DS::MCLAPPD>;
-#pragma link C++ class vector<RAT::DS::MCPhoton>;
-#pragma link C++ class vector<RAT::DS::Calib>;
-#pragma link C++ class vector<RAT::DS::EV>;
-#pragma link C++ class vector<RAT::DS::PMT>;
-#pragma link C++ class vector<RAT::DS::LAPPD>;
-#pragma link C++ class vector<RAT::DS::LAPPDHit>;
-#pragma link C++ class vector< pair<string, int> >;
-#pragma link C++ class vector<RAT::DBTable*>;
-
-#pragma link C++ class vector<RAT::TrackNode*>;
-#pragma link C++ class vector<RAT::DS::EV*>;
-#pragma link C++ class vector<RAT::DS::PMT*>;
-#pragma link C++ class vector<RAT::DS::LAPPD*>;
-#pragma link C++ class vector<RAT::DS::LAPPDHit*>;
-#pragma link C++ class vector<RAT::DS::MCParticle*>;
-#pragma link C++ class vector<RAT::DS::MCTrack*>;
-#pragma link C++ class vector<RAT::DS::MCPMT*>;
-#pragma link C++ class vector<RAT::DS::MCLAPPD*>;
-#pragma link C++ class vector<RAT::DS::MCPhoton*>;
-#pragma link C++ class vector<RAT::DS::MCTrackStep*>;
-
-#endif
 
